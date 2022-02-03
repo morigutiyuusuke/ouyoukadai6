d4c70b5eb29b258d327e9c8bc298e86cffbeda70
EC2 Default User <ec2-user@ip-172-31-84-209.ec2.internal>
2022-02-03 10:17:56 +0000

    コメント

--- Gemfile
+++ Gemfile
@@ -57,3 +57,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'bootstrap', '~> 4.5'
 gem 'jquery-rails'
+gem 'kaminari','~> 1.2.1'
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,13 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @newbook = Book.new
+    @book_comment = BookComment.new
   end
 
   def index
-    @books = Book.all
+    #@books = Book.all
     @book = Book.new
+    @books = Book.page(params[:page]).reverse_order
   end
 
   def create
