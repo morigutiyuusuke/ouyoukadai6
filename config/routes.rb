f69e4fe1ce78e0d1396948be61cba0af738b6576
EC2 Default User <ec2-user@ip-172-31-84-209.ec2.internal>
2022-02-06 08:56:50 +0000

    応用課題5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'searches/search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
@@ -13,4 +14,5 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+    get 'search', to: 'searches#search', as: 'search'
   end
